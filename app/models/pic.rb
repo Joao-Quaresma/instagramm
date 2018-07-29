f65827defe19f1d874e1dc7bff7c56e22d664c11
Joao Quaresma <joaoquaresma@Joaos-MacBook-Pro-3.local>
2018-07-29 17:53:23 +0100

    paperclip gem and uploading pictures

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -3,4 +3,6 @@
 # pics class
 class Pic < ApplicationRecord
   belongs_to :user
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
